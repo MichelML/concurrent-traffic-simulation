66abb88449d5d601b1c32d5c43737e5b48c8d015
michelml <michmoreau.l@gmail.com>
2021-05-11 21:06:53 -0400

    format

--- tutorials/example_1.cpp
+++ tutorials/example_1.cpp
@@ -1,58 +1,52 @@
+#include <algorithm>
+#include <future>
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <future>
-#include<algorithm>
 
-class Vehicle
-{
+class Vehicle {
 public:
-    Vehicle(int id) : _id(id) {}
+  Vehicle(int id) : _id(id) {}
 
 private:
-    int _id;
+  int _id;
 };
 
-class WaitingVehicles
-{
+class WaitingVehicles {
 public:
-    WaitingVehicles() : _tmpVehicles(0) {}
+  WaitingVehicles() : _tmpVehicles(0) {}
 
-    // getters / setters
-    void printSize()
-    {
-        std::cout << "#vehicles = " << _tmpVehicles << std::endl;
-    }
+  // getters / setters
+  void printSize() { std::cout << "#vehicles = " << _tmpVehicles << std::endl; }
 
-    // typical behaviour methods
-    void pushBack(Vehicle &&v)
-    {
-        //_vehicles.push_back(std::move(v)); // data race would cause an exception
-        int oldNum = _tmpVehicles;
-        std::this_thread::sleep_for(std::chrono::microseconds(1)); // wait deliberately to expose the data race
-        _tmpVehicles = oldNum + 1;
-    }
+  // typical behaviour methods
+  void pushBack(Vehicle &&v) {
+    //_vehicles.push_back(std::move(v)); // data race would cause an exception
+    int oldNum = _tmpVehicles;
+    std::this_thread::sleep_for(std::chrono::microseconds(
+        1)); // wait deliberately to expose the data race
+    _tmpVehicles = oldNum + 1;
+  }
 
 private:
-    std::vector<Vehicle> _vehicles; // list of all vehicles waiting to enter this intersection
-    int _tmpVehicles; 
+  std::vector<Vehicle>
+      _vehicles; // list of all vehicles waiting to enter this intersection
+  int _tmpVehicles;
 };
 
-int main()
-{
-    std::shared_ptr<WaitingVehicles> queue(new WaitingVehicles); 
-    std::vector<std::future<void>> futures;
-    for (int i = 0; i < 1000; ++i)
-    {
-        Vehicle v(i);
-        futures.emplace_back(std::async(std::launch::async, &WaitingVehicles::pushBack, queue, std::move(v)));
-    }
+int main() {
+  std::shared_ptr<WaitingVehicles> queue(new WaitingVehicles);
+  std::vector<std::future<void>> futures;
+  for (int i = 0; i < 1000; ++i) {
+    Vehicle v(i);
+    futures.emplace_back(std::async(
+        std::launch::async, &WaitingVehicles::pushBack, queue, std::move(v)));
+  }
 
-    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
-        ftr.wait();
-    });
+  std::for_each(futures.begin(), futures.end(),
+                [](std::future<void> &ftr) { ftr.wait(); });
 
-    queue->printSize();
+  queue->printSize();
 
-    return 0;
+  return 0;
 }
